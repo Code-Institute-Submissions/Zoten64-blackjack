7d638c259ecf5cf7e7292dcdb6a278319034890d
Zoten64 <forjavligratta@gmail.com>
2024-02-14 18:19:26 +0100

    Feat: Add more unit tests to test 3 functions. See test_run.py

--- test_run.py
+++ test_run.py
@@ -2,7 +2,21 @@ import unittest
 import run
 
 class test_run_py(unittest.TestCase):
-    
+    """Unit tests"""
     def test_calc_card_value(self):
+        """Tests if the card value calculation is correct"""
         result = run.game.calc_value(["A♣", "10♣"], run.card_value)
         self.assertEqual(result, 21)
+
+    def test_generate_deck(self):
+        """
+        Tests the generate deck function to make sure it 
+        doesn't return none
+        """
+        result = run.game.generate_deck(1)
+        self.assertIsNotNone(result)
+
+    def test_hash_password(self):
+        """Tests that password hashing doesn't return none"""
+        result = run.account.hash_password("test")
+        self.assertIsNotNone(result)
